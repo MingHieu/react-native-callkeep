3a24c2d02616b0649fc5e853aac1b07dbd3c3b70
MingHieu <77161145+MingHieu@users.noreply.github.com>
2022-12-05 17:18:32 +0700

    fix send error start call event

--- ios/RNCallKeep/RNCallKeep.m
+++ ios/RNCallKeep/RNCallKeep.m
@@ -1013,7 +1013,7 @@ continueUserActivity:(NSUserActivity *)userActivity
     }
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
     dispatch_after(popTime, dispatch_get_main_queue(), ^{
-        [self sendEventWithName:RNCallKeepDidReceiveStartCallAction body:userInfo];
+        [self sendEventWithNameWrapper:RNCallKeepDidReceiveStartCallAction body:userInfo];
     });
 }
 
